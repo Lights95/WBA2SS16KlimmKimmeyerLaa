0c9c56581b893076137c90d432125e686e839335
Marvin Klimm <marvin_christian.klimm@smail.th-koeln.de>
2016-04-30 15:18:36 +0200

    KLIMM-kickoff-Aufgabe3

--- klimm/Aufgaben0Kickoff/aufgabe.js
+++ klimm/Aufgaben0Kickoff/aufgabe.js
@@ -14,4 +14,30 @@ fs.readFile('wolkenkratzer.json', function (err, data) {
      console.log("Hoehe: " + chalk.green(wolkenkratzer.wolkenkratzer[i].hoehe + "m"));
      console.log("--------------------");
    }
+
+   var wolkenkratzer_sortiert = {};
+
+   wolkenkratzer_sortiert.wolkenkratzer = wolkenkratzer.wolkenkratzer.sort(function (a, b) {
+      if (a.hoehe > b.hoehe) {
+        return 1;
+      }
+      if (a.hoehe < b.hoehe) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+  });
+
+  fs.writeFile('wolkenkratzer_sortiert.json', JSON.stringify(wolkenkratzer_sortiert, null, 2),  function(err) {
+     if (err) {
+         return console.error(err);
+     }
+     console.log("Geschrieben!");
+     for (i= 0; i<wolkenkratzerAnzahl; i++) {
+       console.log("Name: "  + chalk.red(wolkenkratzer_sortiert.wolkenkratzer[i].name));
+       console.log("Stadt: " + chalk.blue(wolkenkratzer_sortiert.wolkenkratzer[i].stadt));
+       console.log("Hoehe: " + chalk.green(wolkenkratzer_sortiert.wolkenkratzer[i].hoehe + "m"));
+       console.log("--------------------");
+     }
+  });
 });
