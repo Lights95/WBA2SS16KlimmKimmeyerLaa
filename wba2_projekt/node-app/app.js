6086bc79a6e8d6ebfe3e1cd39d6b3ab0cda278b8
jkimmeyer <jkimmeyer@gmail.com>
2016-06-04 11:52:32 +0200

    Neue Userfunktionen hinzugefügt, Beschreibungen geaddet

--- wba2_projekt/node-app/app.js
+++ wba2_projekt/node-app/app.js
@@ -12,7 +12,7 @@ var user = [
   {name: "Lena", id : 2}
 ]
 
-
+/*API*/
 app.get('/', function(req, res){
   res.send('API für Songabstimmung');
 });
@@ -20,47 +20,64 @@ app.get('/', function(req, res){
 /*User*/
 /*User anlegen*/
 app.post('/users', jsonParser, function(req, res){
+    /*Filtert alle User*/
     db.keys('user:*', function(err, keys){
+        /*Gibt alle User aus der DB zurück*/
         db.mget(keys, function(err, users){
+            /*Überprüft ob Variable einen Wert hat*/
             if(users===undefined){
                 users =[];
             }
+            /*Gibt neues Array an User zurück, welches alle User enthält*/
             users=users.map(function(user){
                 return JSON.parse(user);
             });
             var gesetzt= false;
+            
+            /*Überprüft, ob der neue Nutzername vorhanden ist*/
             users.forEach(function(user){
                 if(user.name === req.body.name) gesetzt=true;
             });
+            
+            
             if(gesetzt){
                 return res.status(401).json({message : "Username bereits vergeben."})
             }
-            db.incr('userIDs', function(err, id){
-                var user = req.body();
-                user.id= id;
-                user.groups= [];
-                db.set('user:' + user.id, JSON.stringify(user), function(err, newUser){
-                    res.type('plain').json(user);
+            /*Erstellt neuen User in der Datenbank*/
+            db.incr('id:users', function(err, id){
+                var newUser = req.body();
+                newUser.id= id;
+                db.set('user:' + user.id, JSON.stringify(newUser), function(err, newUser){
+                    /*neuer User wird als normaler Text zurückgegeben*/ 
+                    res.type('plain').json(newUser);
                 });
             });
         });
     });
 });
 
-
+/*User ausgeben*/
 app.get('/users', function(req, res){
-  res.status(200).json(user);
+  db.keys('user:*', function(err, keys){
+     db.mget(key,function(err, users){
+        res.type('json').send 
+     }); 
+  });
 });
 
+/*Bestimmten User ausgeben*/
 app.get('/users/:id', function(req, res){
-  var userID = req.params.id;
-
-  if (userID < user.length && user[userID].name != 0) {
-    res.status(200).json(user[userID]);
-  }
-  else res.status(404).end();
+   db.get('user;'+req.params.id, function(err,rep){
+       if(rep){
+           res.type('json').send(rep);
+       }
+       else{
+           res.status(404).type('plain').send('Der User mit der ID' + req.params.id +'ist nicht vorhanden.');
+       }
+   });
 });
 
+/*User löschen*/
 app.delete('/users/:id', function(req, res){
   var userID = req.params.id;
 
